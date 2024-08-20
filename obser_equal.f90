321b44066c092eae621f2de5351ae50157d02498
YinkaiYu <yuyk6@mail2.sysu.edu.cn>
2024-08-21 04:23:49 +0800

    增加了用于计算binder ratio的观测量

--- obser_equal.f90
+++ obser_equal.f90
@@ -4,6 +4,7 @@ module ObserEqual_mod
     implicit none
     
     type, public :: ObserEqual
+        real(kind=8) :: Phi2, Phi4
         real(kind=8), dimension(:), allocatable :: den_occ
         real(kind=8), dimension(:,:), allocatable :: bond_occ, spin_occ
         real(kind=8) :: el_ke(Nbond), density, spin_avg(Nboson), spin_order(Nboson), diam
@@ -31,6 +32,8 @@ contains
     subroutine Obs_equal_reset(this)
         class(ObserEqual), intent(inout) :: this
         this%bond_occ = dcmplx(0.d0, 0.d0)
+        this%Phi2 = 0.d0
+        this%Phi4 = 0.d0
         this%spin_occ = 0.d0
         this%den_occ = 0.d0
         this%density = 0.d0
@@ -54,6 +57,8 @@ contains
         this%diam = znorm * this%diam
         this%spin_avg = znorm * this%spin_avg
         this%spin_order = znorm * this%spin_order
+        this%Phi2 = znorm * this%Phi2
+        this%Phi4 = znorm * this%Phi4
         return
     end subroutine Obs_equal_ave
     
@@ -65,8 +70,8 @@ contains
 ! Local: 
         complex(kind=8), dimension(Ndim, Ndim) :: Grupc, Grup
         real(kind=8), dimension(Ndim) :: tmpd
-        integer :: i, ii, no, ns, i1, nf, sign, iy
-        real(kind=8) :: tmp, Ai
+        integer :: i, ii, no, ns, i1, nf, sign, iy, nt
+        real(kind=8) :: tmp, Ai, Phisum(Nboson)
         complex(kind=8) :: phase_p, phase_m
         
         Grup = Prop%Gr !   Gr(i, j) = <c_i c^+_j >
@@ -75,16 +80,16 @@ contains
         do i = 1, Ndim
             tmpd(i) = real(Grupc(i, i) + dconjg(Grupc(i, i)))
         enddo
-!   Density, spin and bond density
+!   Density, SDW(spin) and bond density
 		do i = 1, Ndim
             ii = Latt%o_list(i, 1)
             no = Latt%o_list(i, 2)
             if (Latt%b_list(ii, 2) == 1) sign = 1
             if (Latt%b_list(ii, 2) == 2) sign = -1
-            do ns = 1, Nboson
-                this%spin_occ(ns, ii) = this%spin_occ(ns, ii) + NsigL_K%phi(ns, ii, ntau) * sign
-                this%spin_avg(ns) = this%spin_avg(ns) + NsigL_K%phi(ns, ii, ntau) / dble(Lq)
-                this%spin_order(ns) = this%spin_order(ns) + NsigL_K%phi(ns, ii, ntau) * sign / dble(Lq)
+            do nb = 1, Nboson
+                this%spin_occ(nb, ii) = this%spin_occ(nb, ii) + NsigL_K%phi(nb, ii, ntau) * sign
+                this%spin_avg(nb) = this%spin_avg(nb) + NsigL_K%phi(nb, ii, ntau) / dble(Lq)
+                this%spin_order(nb) = this%spin_order(nb) + NsigL_K%phi(nb, ii, ntau) * sign / dble(Lq)
             enddo
             this%den_occ(ii) = this%den_occ(ii) + tmpd(i)
             this%density = this%density + tmpd(i) / dble(Lq)
@@ -102,6 +107,23 @@ contains
                 endif
             enddo
         enddo
+
+        ! \Phi=\sum \phi(\tau,x,y)
+        Phisum = 0.d0
+        do nb = 1, Nboson
+            do i = 1, Ndim
+                ii = Latt%o_list(i, 1)
+                if (Latt%b_list(ii, 2) == 1) sign = 1
+                if (Latt%b_list(ii, 2) == 2) sign = -1
+                do nt = 1, Ltrot
+                    Phisum(nb) = Phisum(nb) + NsigL_K%phi(nb, ii, nt) * sign
+                enddo
+            enddo
+        enddo
+        ! \Phi^2 and \Phi^4
+        this%Phi2 = this%Phi2 + sqr_vec(Phisum)
+        this%Phi4 = this%Phi4 + sqr_vec(Phisum) * sqr_vec(Phisum)
+
         return
     end subroutine Obs_equal_calc
 end module ObserEqual_mod
